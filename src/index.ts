339252e70c9ad44ee615550cdf1817f3597ff4d9
Mr D <fechnologies@gmail.com>
2024-01-12 19:06:13 +0100

    added documentation and list ordering

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,13 @@ import { locs } from "./locs";
 
 export class mCountry{
 
-  static getAllCountries() {
+  /**
+ * Retrieves an array of all countries (Only supports Nigeria for now). 
+ * Set oderedList to true if you want the data ordered 
+ *
+ * @returns {mLoc[]} An array of mLoc objects representing countries.
+ */
+  static getAllCountries(oderedList?:boolean) {
     const countries:mLoc[] = []
     for (const countryCode in locs) {
       if (locs.hasOwnProperty(countryCode)) {
@@ -15,9 +21,17 @@ export class mCountry{
         countries.push(new mLoc(countryCode,country))
       }
     }
+    if(oderedList){
+      return countries.slice().sort((a, b) => a.getName().localeCompare(b.getName()));
+    }
     return countries;
   }
 
+  /**
+ * Retrieves a country by its code
+ *
+ * @returns {mLoc}
+ */
   static getCountryByCode(countryCode:string) {
     if (locs[countryCode]) {
       const acountry = locs[countryCode];
@@ -35,7 +49,13 @@ export class mCountry{
 
 export class mState{
 
-  static getStatesByCountry(countryCode:string) {
+  /**
+ * Retrieves an array of all states in the provided country code.
+ * Set oderedList to true if you want the data ordered 
+ *
+ * @returns {mLoc[]} An array of mLoc objects representing states.
+ */
+  static getStatesByCountry(countryCode:string,oderedList?:boolean) {
     const states:mLoc[] = []
     if(locs[countryCode]){
       const nLoc = locs[countryCode]['states']
@@ -50,9 +70,17 @@ export class mState{
         }
       }
     }
+    if(oderedList){
+      return states.slice().sort((a, b) => a.getName().localeCompare(b.getName()));
+    }
     return states;
   }
 
+  /**
+ * Retrieves a state by its code and countryCode
+ *
+ * @returns {mLoc}
+ */
   static getStateByCode(countryCode:string,stateCode:string) {
     if(locs[countryCode]){
       const nLoc = locs[countryCode]['states']
@@ -72,7 +100,13 @@ export class mState{
 
 export class mLga{
 
-  static getLgasByState(countryCode:string,stateCode:string) {
+  /**
+ * Retrieves an array of all LGAs in the specified country & state.
+ * Set oderedList to true if you want the data ordered 
+ *
+ * @returns {mLoc[]} An array of mLoc objects representing LGAs.
+ */
+  static getLgasByState(countryCode:string,stateCode:string,oderedList?:boolean) {
     const lgas:mLoc[] = []
     if(locs[countryCode]){
       const nLoc = locs[countryCode]['states']
@@ -86,9 +120,17 @@ export class mLga{
         }
       }
     }
+    if(oderedList){
+      return lgas.slice().sort((a, b) => a.getName().localeCompare(b.getName()));
+    }
     return lgas;
   }
 
+  /**
+ * Retrieves an LGA by its code, state code and country code
+ *
+ * @returns {mLoc}
+ */
   static getLgaByCode(countryCode:string,stateCode:string, lgaCode:string) {
     if(locs[countryCode]){
       const nLoc = locs[countryCode]['states']
